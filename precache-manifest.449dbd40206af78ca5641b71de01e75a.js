e5f187729fd44341974abe55794d8d159568e583
Aren <reactDOM.render@yandex.ru>
2020-07-24 16:52:56 +0300

    Updates

---
+++ precache-manifest.449dbd40206af78ca5641b71de01e75a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452774fb797af01b9f4c1487fa88249c",
+    "url": "/prachka/index.html"
+  },
+  {
+    "revision": "e7db4febe8d7ed97ceb6",
+    "url": "/prachka/static/css/main.90abaa63.chunk.css"
+  },
+  {
+    "revision": "4296c0a76b03fdb2c207",
+    "url": "/prachka/static/js/2.ced13150.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/prachka/static/js/2.ced13150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7db4febe8d7ed97ceb6",
+    "url": "/prachka/static/js/main.1206af88.chunk.js"
+  },
+  {
+    "revision": "03fe9327b782738a0576",
+    "url": "/prachka/static/js/runtime-main.65437ee1.js"
+  },
+  {
+    "revision": "c2103366da4447ab9b8c52eed2e41a9f",
+    "url": "/prachka/static/media/g-1.c2103366.jpg"
+  },
+  {
+    "revision": "44f6aa9118170184ea137503ec29ff04",
+    "url": "/prachka/static/media/g-2.44f6aa91.jpg"
+  },
+  {
+    "revision": "3f4ec31aa0f51c297aa52a7e75600643",
+    "url": "/prachka/static/media/g-3.3f4ec31a.jpg"
+  },
+  {
+    "revision": "8e13aebda0492215b6cdc5b35c07592b",
+    "url": "/prachka/static/media/g-4.8e13aebd.jpg"
+  },
+  {
+    "revision": "22157bae3a716b01c8a85a393334b2c9",
+    "url": "/prachka/static/media/g-5.22157bae.jpg"
+  },
+  {
+    "revision": "459ac31d00e00be56bea033095ca6eb8",
+    "url": "/prachka/static/media/logo.459ac31d.jpg"
+  }
+]);
